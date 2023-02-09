f67fd90dc6bd17c66b86fd49c1b3bada3e6bf5f0
PavelBabin <pavel-babin@yandex.ru>
2023-02-09 19:46:55 +0500

    Lesson 10

---
+++ lesson_10/data.js
@@ -0,0 +1,64 @@
+const dataInfo = `
+[
+  {
+    "id": 87414,
+    "img_answer": "https://i.imgur.com/PM9gjkA.jpg",
+    "answer": "the Harlem Globetrotters",
+    "question": "Go, Man, Go! tells the story of Abe Saperstein the formation of this team",
+    "value": 200,
+    "airdate": "2003-03-12T19:00:00.000Z",
+    "created_at": "2022-12-30T19:17:15.456Z",
+    "updated_at": "2022-12-30T19:17:15.456Z",
+    "category_id": 8971,
+    "game_id": 2805,
+    "invalid_count": null,
+    "category": {
+      "id": 8971,
+      "title": "basketball movies",
+      "created_at": "2022-12-30T19:17:15.415Z",
+      "updated_at": "2022-12-30T19:17:15.415Z",
+      "clues_count": 5
+    }
+  },
+  {
+    "id": 23553,
+    "img_answer": "https://www.kompas-rf.ru/wp-content/uploads/2020/04/2727460695-1024x966.jpg",
+    "answer": "Stalin",
+    "question": "On Halloween night 1961 the body of this Georgian was removed from display in Lenin's tomb",
+    "value": 300,
+    "airdate": "1991-05-28T19:00:00.000Z",
+    "created_at": "2022-12-30T18:47:15.209Z",
+    "updated_at": "2022-12-30T18:47:15.209Z",
+    "category_id": 959,
+    "game_id": 7380,
+    "invalid_count": null,
+    "category": {
+      "id": 959,
+      "title": "the 20th century",
+      "created_at": "2022-12-30T18:40:25.945Z",
+      "updated_at": "2022-12-30T18:40:25.945Z",
+      "clues_count": 139
+    }
+  },
+  {
+    "id": 107863,
+    "img_answer": "https://upload.wikimedia.org/wikipedia/commons/9/96/RapidsPR3.jpg",
+    "answer": "Rapids",
+    "question": "In city names, they can be Grand or Cedar",
+    "value": 200,
+    "airdate": "2006-03-24T19:00:00.000Z",
+    "created_at": "2022-12-30T19:37:22.188Z",
+    "updated_at": "2022-12-30T19:37:22.188Z",
+    "category_id": 11720,
+    "game_id": 858,
+    "invalid_count": null,
+    "category": {
+      "id": 11720,
+      "title": "rap sheet",
+      "created_at": "2022-12-30T19:37:21.741Z",
+      "updated_at": "2022-12-30T19:37:21.741Z",
+      "clues_count": 5
+    }
+  }
+]
+`
