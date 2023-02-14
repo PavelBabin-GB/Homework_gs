bdc1afc2218badc7daa1d03d1bca58c23f6cbbfc
PavelBabin <pavel-babin@yandex.ru>
2023-02-14 16:55:05 +0500

    Lesson 11 correction

--- lesson_11/data.js
+++ lesson_11/data.js
@@ -4,6 +4,7 @@ const dataProducts = `[
     "title": "ELLERY X M'O CAPSULE",
     "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
     "price": "60"
+    
   },
   {"picture": "img/image_3.jpg",
     "title": "ELLERY X M'O CAPSULE",
